5515633c667cf7df9be25bf12e1312a37b95e0a9
Christerle Nounoute <christerlesn@gmail.com>
2020-10-24 03:26:05 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,3 +8,10 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
   binding.pry
 end
+
+create_project_hash
+
+#<li class="project grid_4">
+# projects: kickstarter.css("li.project.grid_4").first
+# title: project.css("h2.bbcard_name strong a").text
+#image : project.css("div.project-thumbnail a img").attribute("src").value
