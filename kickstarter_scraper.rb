8a1a5ee72eb0fcf7b0d2b160cc9450e5eb2326af
Christerle Nounoute <christerlesn@gmail.com>
2020-10-24 03:13:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,10 @@
 # require libraries/modules here
 require 'nokogiri'
+require 'pry'
 
 kickstarter = Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
 
 def create_project_hash
   # write your code here
 end
+binding.pry
